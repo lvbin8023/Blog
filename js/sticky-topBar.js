a41c5ff32d10f58847fd688ec38223a977ee7c0c
lvbin <472245767@qq.com>
2018-12-06 11:58:56 +0800

    代码模块化

--- js/sticky-topBar.js
+++ js/sticky-topBar.js
@@ -1,9 +1,11 @@
-// 导航栏动画
-let topNavBar = document.getElementById('topNavBar');
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-        topNavBar.classList.add('sticky');
-    } else {
-        topNavBar.classList.remove('sticky');
-    }
-});
+!function () {
+    // 导航栏动画
+    let topNavBar = document.getElementById('topNavBar');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            topNavBar.classList.add('sticky');
+        } else {
+            topNavBar.classList.remove('sticky');
+        }
+    });
+}.call();
